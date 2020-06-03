d8ff8f1ad8cb707a25f1a032a2dd4a624ab6e354
Norbert Horgas <norbert.horgas@externals.adidas.com>
2020-06-03 11:29:17 +0200

    weather: update city of user

--- src/controllers/weather.controller.ts
+++ src/controllers/weather.controller.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { Request, Response } from "express";
+import { MongooseDocument } from "mongoose";
 
 import { ErrorMessages } from "../constants/errorMessages";
+
+interface AuthenticatedRequest extends Request {
+  user: MongooseDocument;
+}
 
 export const getWeatherInfoByCity = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
+    await (req as AuthenticatedRequest).user.updateOne({ city });
+
     const weatherApiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API}&units=metric`;
 
     try {
